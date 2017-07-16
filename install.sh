f6c4dec5cfa13917435692ce30bada44daf702c7
Chad Homan <choman@gmail.com>
2017-07-16 13:48:53 -0500

    added ssh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ sudo add-apt-repository ppa:libreoffice/ppa
 sudo add-apt-repository ppa:git-core/ppa
 
 sudo apt-get update
-sudo apt-get install git apt-fast di tmux
+sudo apt-get install git apt-fast di tmux ssh
 sudo apt-fast dist-upgrade -y
