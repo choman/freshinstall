d6ccf5db862358b74f21b4f4102ef8c885709503
Chad Homan <choman@gmail.com>
2017-07-19 21:36:17 -0500

    add tms

--- install.sh
+++ install.sh
@@ -3,7 +3,15 @@
 sudo add-apt-repository -y ppa:saiarcot895/myppa
 sudo add-apt-repository -y ppa:libreoffice/ppa
 sudo add-apt-repository -y ppa:git-core/ppa
+sudo add-apt-repository -y ppa:tmsu/ppa
 
 sudo apt-get update
-sudo apt-get install -y git apt-fast di tmux ssh
+sudo apt-get install -y git apt-fast di tmux ssh tmsu
 sudo apt-fast dist-upgrade -y
+
+#
+# Add vbox
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+sudo apt-get update
+sudo apt-fast install virtualbox-5.1
+
